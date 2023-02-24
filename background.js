7fc9f830b2f47f5de1ef964f99b5ae9482254282
Alan Files <alan@triplewhale.com>
2023-02-24 12:37:13 -0500

    can't stop refactoring'

--- background.js
+++ background.js
@@ -2,46 +2,45 @@ chrome.storage.local.get('mode', function(items) {
     const mode = items.mode;
 
     if (mode === 'record') {
-      recordingFunction();
+        if(chrome.webRequest.onCompleted.hasListener(responsePlaybackFunction)){
+            chrome.webRequest.onCompleted.removeListener(responsePlaybackFunction);
+        }
+        chrome.webRequest.onCompleted.addListener(listeningFunction, { urls: ["<all_urls>"] }, ["responseHeaders"]);
     } else if (mode === 'playback') {
-      playbackFunction();
+        if(chrome.webRequest.onBeforeRequest.hasListener(listeningFunction)){
+            chrome.webRequest.onBeforeRequest.removeListener(listeningFunction);
+        }
+        chrome.webRequest.onBeforeRequest.addListener(responsePlaybackFunction, { urls: ["<all_urls>"] }, ["blocking"]);
     }
   });
-  
 
-  const recordingFunction = () => {
-    // Record mode: capture request and response data
-    chrome.webRequest.onCompleted.addListener(function(details) {
-        const response = details;
-        if (response.statusCode >= 200 && response.statusCode < 300) {
-          const url = response.url;
-          const request = new XMLHttpRequest();
-          request.open("GET", url, true);
-          request.onreadystatechange = function() {
-            if (request.readyState === 4) {
-              const response = request.responseText;
-              chrome.storage.local.get('recordedRequests', function(items) {
-                const recordedRequests = items.recordedRequests || {};
-                recordedRequests[url] = response;
-                chrome.storage.local.set({ recordedRequests: recordedRequests });
-              });
-            }
-          };
-          request.send();
+  const recordingFunction = function(details) {
+    const response = details;
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      const url = response.url;
+      const request = new XMLHttpRequest();
+      request.open("GET", url, true);
+      request.onreadystatechange = function() {
+        if (request.readyState === 4) {
+          const response = request.responseText;
+          chrome.storage.local.get('recordedRequests', function(items) {
+            const recordedRequests = items.recordedRequests || {};
+            recordedRequests[url] = response;
+            chrome.storage.local.set({ recordedRequests: recordedRequests });
+          });
         }
-      }, { urls: ["<all_urls>"] }, ["responseHeaders"]);
+      };
+      request.send();
+    }
   }
-
-  const playbackFunction = () => {
-    // Playback mode: modify response data for matching URLs
-    chrome.webRequest.onBeforeRequest.addListener(function(details) {
-        const response = details;
-        chrome.storage.local.get('recordedRequests', function(items) {
-          const recordedRequests = items.recordedRequests || {};
-          const response = recordedRequests[response.url];
-          if (response) {
-            return { redirectUrl: "data:text/html;charset=utf-8," + encodeURIComponent(response) };
-          }
-        });
-      }, { urls: ["<all_urls>"] }, ["blocking"]);
+  
+  const playbackFunction = function(details) {
+    const response = details;
+    chrome.storage.local.get('recordedRequests', function(items) {
+      const recordedRequests = items.recordedRequests || {};
+      const response = recordedRequests[response.url];
+      if (response) {
+        return { redirectUrl: "data:text/html;charset=utf-8," + encodeURIComponent(response) };
+      }
+    });
   }
