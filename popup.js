92c36c4b423c8252873fdc2f1c8893c3b6acb413
Mike Weaver <120399148+weaverwhale@users.noreply.github.com>
2023-02-27 16:02:11 -0500

    Downgrade to V2; working example (#1)

--- popup.js
+++ popup.js
@@ -1,11 +1,32 @@
-document.getElementById('record').addEventListener('click', function() {
-    chrome.storage.local.set({ mode: 'record' });
-    document.getElementById('record').classList.add("active");
-    document.getElementById('playback').classList.remove("active");
-  });
-  
-document.getElementById('playback').addEventListener('click', function() {
-    chrome.storage.local.set({ mode: 'playback' });
-    document.getElementById('playback').classList.add("active");
-    document.getElementById('record').classList.remove("active");
-});
+document.getElementById('record').addEventListener('click', function () {
+  chrome.storage.local.set({ mode: 'record' })
+  document.getElementById('record').classList.add('active')
+  document.getElementById('playback').classList.remove('active')
+})
+
+document.getElementById('playback').addEventListener('click', function () {
+  chrome.storage.local.set({ mode: 'playback' })
+  document.getElementById('playback').classList.add('active')
+  document.getElementById('record').classList.remove('active')
+})
+
+document.getElementById('reset').addEventListener('click', function () {
+  chrome.storage.local.clear()
+  document.getElementById('playback').classList.remove('active')
+  document.getElementById('record').classList.remove('active')
+})
+
+document.getElementById('save').addEventListener('click', function () {
+  // SEND TO FIREBASE
+  console.log('send to firebase')
+})
+
+chrome.storage.local.get('mode', function (items) {
+  const mode = items.mode
+
+  if (mode === 'record') {
+    document.getElementById('record').classList.add('active')
+  } else if (mode === 'playback') {
+    document.getElementById('playback').classList.add('active')
+  }
+})
