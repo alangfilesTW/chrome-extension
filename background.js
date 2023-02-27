1255cbd3fc841480e2c73644e2cda13814253656
Mike Weaver <michael@triplewhale.com>
2023-02-27 15:26:03 -0500

    Downgrade to V2; working example

--- background.js
+++ background.js
@@ -1,46 +1,98 @@
-chrome.storage.local.get('mode', function(items) {
-    const mode = items.mode;
+function getMode() {
+  chrome.storage.local.get('mode', function (items) {
+    const mode = items.mode
 
     if (mode === 'record') {
-        if(chrome.webRequest.onCompleted.hasListener(responsePlaybackFunction)){
-            chrome.webRequest.onCompleted.removeListener(responsePlaybackFunction);
-        }
-        chrome.webRequest.onCompleted.addListener(listeningFunction, { urls: ["<all_urls>"] }, ["responseHeaders"]);
-    } else if (mode === 'playback') {
-        if(chrome.webRequest.onBeforeRequest.hasListener(listeningFunction)){
-            chrome.webRequest.onBeforeRequest.removeListener(listeningFunction);
-        }
-        chrome.webRequest.onBeforeRequest.addListener(responsePlaybackFunction, { urls: ["<all_urls>"] }, ["blocking"]);
-    }
-  });
+      console.log('Record mode')
+      chrome.webRequest.onCompleted.removeListener(recordingFunction)
+      chrome.webRequest.onBeforeRequest.removeListener(playbackFunction)
 
-  const recordingFunction = function(details) {
-    const response = details;
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      const url = response.url;
-      const request = new XMLHttpRequest();
-      request.open("GET", url, true);
-      request.onreadystatechange = function() {
-        if (request.readyState === 4) {
-          const response = request.responseText;
-          chrome.storage.local.get('recordedRequests', function(items) {
-            const recordedRequests = items.recordedRequests || {};
-            recordedRequests[url] = response;
-            chrome.storage.local.set({ recordedRequests: recordedRequests });
-          });
-        }
-      };
-      request.send();
+      chrome.webRequest.onCompleted.addListener(
+        recordingFunction,
+        { urls: ['<all_urls>'] },
+        ['responseHeaders'],
+      )
+    } else if (mode === 'playback') {
+      console.log('Playback mode')
+      chrome.webRequest.onCompleted.removeListener(recordingFunction)
+      chrome.webRequest.onBeforeRequest.removeListener(playbackFunction)
+
+      chrome.webRequest.onBeforeRequest.addListener(
+        playbackFunction,
+        { urls: ['<all_urls>'] },
+        ['blocking'],
+      )
+    } else {
+      chrome.webRequest.onCompleted.removeListener(recordingFunction)
+      chrome.webRequest.onBeforeRequest.removeListener(playbackFunction)
+      console.log('Empty state')
+    }
+  })
+}
+
+getMode()
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  for (let key in changes) {
+    if (key === 'mode') {
+      getMode()
     }
   }
-  
-  const playbackFunction = function(details) {
-    const response = details;
-    chrome.storage.local.get('recordedRequests', function(items) {
-      const recordedRequests = items.recordedRequests || {};
-      const response = recordedRequests[response.url];
-      if (response) {
-        return { redirectUrl: "data:text/html;charset=utf-8," + encodeURIComponent(response) };
-      }
-    });
+})
+
+let cachedEndpointRequests = []
+const recordingFunction = function (details) {
+  const response = details
+  const url = response?.url && response.url?.length > 0 ? response.url : false
+
+  if (!url) return
+  if (url.includes('chrome-extension://')) return
+
+  const key = `${response.method}:${response.url}`
+
+  if (
+    url &&
+    response?.statusCode >= 200 &&
+    response?.statusCode < 300 &&
+    !cachedEndpointRequests.includes(key)
+  ) {
+    cachedEndpointRequests.push(key)
+
+    fetch(response.url)
+      .then((response) => response.text())
+      .then((res) => {
+        chrome.storage.local.get('recordedRequests', function (items) {
+          const recordedRequests = items.recordedRequests || {}
+
+          if (res) {
+            recordedRequests[key] = res
+            chrome.storage.local.set({ recordedRequests: recordedRequests })
+            console.log(`${response.method} request recorded: ${response.url}`)
+          }
+        })
+      })
   }
+}
+
+const playbackFunction = function (res) {
+  chrome.storage.local.get('recordedRequests', function (items) {
+    const recordedRequests = items.recordedRequests || {}
+    const response = recordedRequests[`${res.method}:${res.url}`]
+    if (response) {
+      console.log(`${res.method} request intercepted: ${res.url}`)
+
+      return {
+        redirectUrl:
+          'data:text/html;charset=utf-8,' + encodeURIComponent(response),
+      }
+    }
+  })
+}
+
+const sendRecording = function () {
+  chrome.storage.local.get('recordedRequests', function (items) {
+    const recordedRequests = items.recordedRequests || {}
+
+    // SEND TO FIREBASE
+    console.log(recordedRequests)
+  })
+}
